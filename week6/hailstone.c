54921476649c7fc22813c85dd1b65b568f35c1db
shadielfares <shadi.elfares@gmail.com>
2024-10-28 12:48:01 -0400

    Added comments to 6.2

--- week6/hailstone.c
+++ week6/hailstone.c
@@ -1,13 +1,16 @@
+// This file computes a hailstone sequence
 #include <stdio.h>
 #include <stdlib.h>
 
 int hailstone(long n)
 {
     printf("%ld ", n);
+    // Terminating case
     if (n == 1)
     {
         return 1;
     }
+    // Base cases
     else if (n % 2 == 0)
     {
         return 1 + hailstone(n / 2);
